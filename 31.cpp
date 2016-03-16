449e35d7d5b11c26541b9d97f1c4768982eb6a9b
like1413042029 <like0313@foxmail.com>
2016-03-16 17:53:54 +0800

    Added files via upload

---
+++ 31.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+//函数: 查找子串sub在str中出现的次数--用C++方式--子串可重叠 
+int fun(const std::string& str, const std::string& sub)
+{
+int num = 0;
+for (size_t i=0; (i=str.find(sub,i)) != std::string::npos; num++, i++);
+return num;
+}
+
+
+void main()
+{
+std::string str("Many of my classmates have a computer, I have one too. My father bought it for me as a present when my first year in middle school. He said I can study English with computer. Most of the time, I use computer to search study materials on the internet. I also have some foreign friends on the internet, we can talk in English. Sometimes I play video game with computer after I finish my homework. My computer helps me a lot, it is a good friend to me.");
+std::string sub("computer");
+cout<<fun(str,sub)<<endl;
+
+}
